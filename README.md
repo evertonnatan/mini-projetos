55fba4fa6eea129b37bea131579b1eb47d7872ea
evertonnatan <evertonnatandl@outlook.com>
2022-04-21 21:11:25 +0000

    Primeiro estágio do projeto finalizado

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# mini-projetos
+# Gerador de QR Code
+
+Pequeno projeto para gerar QR codes para links (principalmente). 
+
+### Dependências:
+
+* Pillow (https://pillow.readthedocs.io/en/stable/)
+* qrcode (https://pypi.org/project/qrcode/)
--- codeQR.py
+++ codeQR.py
@@ -1,7 +1,13 @@
 import qrcode
 
 
-link = "https://teams.microsoft.com/l/meetup-join/19%3ameeting_YWExZjkxMmEtZWQ2MC00NjExLWE0YjYtZmVkNjZhMTFmZmQ5%40thread.v2/0?context=%7b%22Tid%22%3a%223590422d-8e59-4036-9245-d6edd8cc0f7a%22%2c%22Oid%22%3a%22c9c39212-e4b6-4a82-a170-c8e336a72aaa%22%7d"
+link = ""
 
 img = qrcode.make(link)
 img.show()
+
+'''
+
+Créditos: Hallison do canal Programação Dinâmica (https://youtu.be/BDhSrdopl-Q) 
+
+'''
